b36af675fd911b80d216210d672679a3b2781d12
qmtma <61971052+qmtma@users.noreply.github.com>
2021-10-09 15:27:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import csv
 
+
 ROOT = 'courses'
 courses = os.listdir(ROOT)
 
 
 def getUsernames():
-    with open(f"{ROOT}/{course}/{level}/classList.csv", 'r') as csvFile:
-        userNames = csvFile.read().strip(',').split()
+    csvFile = open(f"{ROOT}/{course}/{level}/classList.csv", 'r')
+    userNames = csvFile.read().strip(',').split()
     csvFile.close()
     del userNames[0]
     return userNames
@@ -15,13 +16,13 @@ def getUsernames():
 
 
 def getClassInfo():
-    with open(f"{ROOT}/{course}/{level}/classInfo.csv", 'r') as csvFile:
-        classData = csv.reader(csvFile, delimiter=',')
-        for row in classData:
-            if row[0].isdigit():
-                classDuration = row[0]
-            if row[1].isdigit():
-                classPerSem = row[1]
+    csvFile = open(f"{ROOT}/{course}/{level}/classInfo.csv", 'r')
+    classData = csv.reader(csvFile, delimiter=',')
+    for row in classData:
+        if row[0].isdigit():
+            classDuration = row[0]
+        if row[1].isdigit():
+            classPerSem = row[1]
         CI = [classDuration,classPerSem]
         csvFile.close()
         return CI
@@ -30,9 +31,9 @@ def getClassInfo():
 
 def getBBabsence():
     BBAbsence=[]
-    with open(f"{ROOT}/{course}/{level}/attendance_BB.csv", 'r') as csvFile:
-        attendanceBB = csv.reader(csvFile, delimiter=',')
-        for row in attendanceBB:
+    csvFile =  open(f"{ROOT}/{course}/{level}/attendance_BB.csv", 'r')
+    attendanceBB = csv.reader(csvFile, delimiter=',')
+    for row in attendanceBB:
             if row[1] in userList:
                 BBcount =0
                 for item in row:
@@ -51,7 +52,7 @@ def getAttendanceLists():
             AttendanceLists.append(file)
             FileCount += 1
     return AttendanceLists, FileCount
-    pass
+
 
 
 def getCUabsence():
@@ -60,20 +61,20 @@ def getCUabsence():
     for name in userList:
         CUList.update({name:0})
     for lecture in lectures:
-        with open(f"{ROOT}/{course}/{level}/{lecture}") as csvFile:
-            CUData = csv.reader(csvFile, delimiter=",")
-            for row in CUData:
-                for user in CUList:
-                    if user == row[1]:
-                        entry = CUList.get(user)
-                        entry +=1
-                        attendedTime = row[6]
-                        attendedH = attendedTime[0]
-                        attendedM = attendedTime[2:4]
-                        attendedM = int(attendedM) + (int(attendedH) * 60)
-                        if int(attendedM) < (0.75 * int(CI[0])):
-                            entry -= 1
-                        CUList.update({user:entry})
+        csvFile =  open(f"{ROOT}/{course}/{level}/{lecture}")
+        CUData = csv.reader(csvFile, delimiter=",")
+        for row in CUData:
+            for user in CUList:
+                if user == row[1]:
+                    entry = CUList.get(user)
+                    entry += 1
+                    attendedTime = row[6]
+                    attendedH = attendedTime[0]
+                    attendedM = attendedTime[2:4]
+                    attendedM = int(attendedM) + (int(attendedH) * 60)
+                    if int(attendedM) < (0.75 * int(CI[0])):
+                        entry -= 1
+                    CUList.update({user: entry})
     for user in CUList:
         entry = CUList.get(user)
         entry = AttendanceTaken - entry
@@ -81,7 +82,7 @@ def getCUabsence():
     return CUAbsence
 
 
-    pass
+
 
 
 def getCUAPer():
@@ -93,7 +94,7 @@ def getCUAPer():
         list.append(entry)
         i+=1
     return list
-    pass
+
 
 
 def getBBA_per():
@@ -104,7 +105,7 @@ def getBBA_per():
         i+=1
         list.append(round(entry,2))
     return list
-    pass
+
 
 
 def getCCUAbsence():
@@ -115,7 +116,7 @@ def getCCUAbsence():
         i+=1
         list.append(round(entry,2))
     return list
-    pass
+
 
 
 def getCUStatus():
@@ -129,7 +130,6 @@ def getCUStatus():
         else: list.append("OK")
         i+=1
     return list
-    pass
 
 
 def getCBBAbsence():
@@ -158,7 +158,7 @@ def getBBStatus():
             list.append(entry)
         i+=1
     return list
-    pass
+
 
 
 for course in courses:
@@ -178,19 +178,21 @@ for course in courses:
                 CU_Status = getCUStatus()
                 C_BB_Absence = getCBBAbsence()
                 BB_Status = getBBStatus()
-                with open(f"{ROOT}/{course}/CUvsBBReport_{course}_{level}.txt",'w') as WriteFile:
+                WriteFile =  open(f"{ROOT}/{course}/CUvsBBReport_{course}_{level}.txt",'w')
 
-                    WriteFile.write("Username       |CU_Absance No | BB_Absance No | CU_Absance% | BB_Absance% | C_CU_Absance% | CU_Status | C_BB_Absance% | BB_Status\n")
-                    i=0
-                    for user in userList:
-                        len0 = len("Username       ")-len(user)
-                        len1 = len("CU_Absance No ")-len(str(CUAbsence[i]))
-                        len2 = len(" BB_Absance No ")-len(str(BBAbsence[i]))
-                        len3 = len(" CU_Absance% ")-len(str(CUA_Per[i]))
-                        len4 = len(" BB_Absance% ")- len(str(BBA_per[i]))
-                        len5 = len(" C_CU_Absance% ")- len(str(C_CU_Absence[i]))
-                        len6 = len(" CU_Status ") - len(CU_Status[i])
-                        len7 = len(" C_BB_Absance% ") - len(str(C_BB_Absence[i]))
-                        len8 = len(" BB_Status") - len(BB_Status[i])
-                        WriteFile.writelines(f"{user}{(len0*' ')}|{CUAbsence[i]}{(len1*' ')}|{BBAbsence[i]}{(len2*' ')}|{CUA_Per[i]}{(len3*' ')}|{BBA_per[i]}{(len4*' ')}|{C_CU_Absence[i]}{(len5*' ')}|{CU_Status[i]}{(len6*' ')}|{C_BB_Absence[i]}{(len7*' ')}|{BB_Status[i]}\n")
-                        i+=1
+                WriteFile.write("Username       |CU_Absance No | BB_Absance No | CU_Absance% | BB_Absance% | C_CU_Absance% | CU_Status | C_BB_Absance% | BB_Status\n")
+                i=0
+                for user in userList:
+                    len0 = len("Username       ") - len(user)
+                    len1 = len("CU_Absance No ") - len(str(CUAbsence[i]))
+                    len2 = len(" BB_Absance No ") - len(str(BBAbsence[i]))
+                    len3 = len(" CU_Absance% ") - len(str(CUA_Per[i]))
+                    len4 = len(" BB_Absance% ") - len(str(BBA_per[i]))
+                    len5 = len(" C_CU_Absance% ") - len(str(C_CU_Absence[i]))
+                    len6 = len(" CU_Status ") - len(CU_Status[i])
+                    len7 = len(" C_BB_Absance% ") - len(str(C_BB_Absence[i]))
+                    len8 = len(" BB_Status") - len(BB_Status[i])
+                    WriteFile.writelines(
+                        f"{user}{(len0 * ' ')}|{CUAbsence[i]}{(len1 * ' ')}|{BBAbsence[i]}{(len2 * ' ')}|{CUA_Per[i]}{(len3 * ' ')}|{BBA_per[i]}{(len4 * ' ')}|{C_CU_Absence[i]}{(len5 * ' ')}|{CU_Status[i]}{(len6 * ' ')}|{C_BB_Absence[i]}{(len7 * ' ')}|{BB_Status[i]}\n")
+                    i += 1
+                WriteFile.close()
